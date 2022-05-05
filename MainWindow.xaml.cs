9ca64c5053c8a30a3d4e5aa45add84f57d14dcd8
Владислав Гречанник <m2108502@edu.misis.ru>
2022-05-05 21:13:21 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LabaOOP6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly FileStream File = new FileStream("shapes.txt", FileMode.OpenOrCreate);
+        readonly List<Shape> Shapes = new List<Shape>();
+        readonly static Random Random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            File.SetLength(0);
+            StreamWriter sw = new StreamWriter(File);
+            foreach (var shape in Shapes)
+                shape.Save(sw);
+            sw.Close();
+            sw.Dispose();
+        }
+
+        private void Load(object sender, RoutedEventArgs e)
+        {
+            Drawings.Children.Clear();
+            StreamReader sr = new StreamReader(File);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                string[] words = line.Split(' ');
+                Shape shape;
+                switch (words[0])
+                {
+                    case "Квадрат":
+                        shape = new Square(words);
+                        shape.Draw(Drawings);
+                        Shapes.Add(shape);
+                        break;
+                    case "Круг":
+                        shape = new Circle(words);
+                        shape.Draw(Drawings);
+                        Shapes.Add(shape);
+                        break;
+                    case "Треугольник":
+                        shape = new Triangle(words);
+                        shape.Draw(Drawings);
+                        Shapes.Add(shape);
+                        break;
+                    default:
+                        break;
+                }
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            sr.Dispose();
+        }
+
+        private void Create(object sender, RoutedEventArgs e)
+        {
+            int shape = Random.Next(3);
+            int size = Random.Next(20, 100);
+            Point center = new Point(Random.Next((int)Drawings.ActualWidth), Random.Next((int)Drawings.ActualHeight));
+            Color color = Color.FromRgb((byte)Random.Next(256), (byte)Random.Next(256), (byte)Random.Next(256));
+            Shape shapeType;
+            switch (shape)
+            {
+                case 0:
+                    shapeType = new Square(center, color, size);
+                    break;
+                case 1:
+                    shapeType = new Circle(center, color, size);
+                    break;
+                case 2:
+                    shapeType = new Triangle(center, color, size);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            shapeType.Draw(Drawings);
+            Shapes.Add(shapeType);
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace LabaOOP6
+{
+    abstract class Shape
+    {
+        protected Point Center;
+        protected Color Color;
+        protected double Size;
+        protected string Name;
+
+        public abstract double Area();
+
+        public abstract System.Windows.Shapes.Shape Draw(Canvas canvas);
+
+        public abstract bool IsInside(Point point);
+
+        public virtual void ShowInfo()
+        {
+            MessageBox.Show($"{Name} с центром в точке ({Center.X} , {Center.Y}). Площадь: {Area()}");
+        }
+
+        public void Click(object sender, RoutedEventArgs e)
+        {
+            ShowInfo();
+        }
+
+        public void Save(StreamWriter sb)
+        {
+            sb.WriteLine($"{Name} {Color.R} {Color.G} {Color.B} {Size} {Center.X} {Center.Y}");
+        }
+    }
+    class Square: Shape
+    {
+            
+        public Square(Point center, Color color, double size)
+        {
+            Center = center;
+            Color = color;
+            Size = size;
+            Name = "Квадрат";
+        }
+
+        public Square(string[] vs)
+        {
+            Color = Color.FromRgb(byte.Parse(vs[1]), byte.Parse(vs[2]), byte.Parse(vs[3]));
+            Size = double.Parse(vs[4]);
+            Center = new Point(double.Parse(vs[5]), double.Parse(vs[6]));
+            Name = "Квадрат";
+        }
+
+        public override double Area()
+        {
+            return Size * Size;
+        }
+
+        public override System.Windows.Shapes.Shape Draw(Canvas canvas)
+        {
+            var rect = new System.Windows.Shapes.Rectangle
+            {
+                Width = Size,
+                Height = Size,
+                Stroke = new SolidColorBrush(Color),
+                StrokeThickness = 1,
+                Fill = new SolidColorBrush(Color),
+            };
+            rect.MouseLeftButtonUp += Click;
+            Canvas.SetLeft(rect, Center.X - Size / 2);
+            Canvas.SetTop(rect, Center.Y - Size / 2);
+            canvas.Children.Add(rect);
+            return rect;
+        }
+
+        public override bool IsInside(Point point)
+        {
+            return (point.X >= Center.X - Size / 2 && point.X <= Center.X + Size / 2) &&
+                   (point.Y >= Center.Y - Size / 2 && point.Y <= Center.Y + Size / 2);
+        }
+    }
+
+    class Circle: Shape
+    {
+        public Circle(Point center, Color color, double size)
+        {
+            Center = center;
+            Color = color;
+            Size = size;
+            Name = "Круг";
+        }
+
+        public Circle(string[] vs)
+        {
+            Color = Color.FromRgb(byte.Parse(vs[1]), byte.Parse(vs[2]), byte.Parse(vs[3]));
+            Size = double.Parse(vs[4]);
+            Center = new Point(double.Parse(vs[5]), double.Parse(vs[6]));
+            Name = "Круг";
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Size * Size;
+        }
+
+        public override System.Windows.Shapes.Shape Draw(Canvas canvas)
+        {
+            var ellipse = new System.Windows.Shapes.Ellipse
+            {
+                Width = Size,
+                Height = Size,
+                Stroke = new SolidColorBrush(Color),
+                StrokeThickness = 1, 
+                Fill = new SolidColorBrush(Color),
+            };
+            ellipse.MouseLeftButtonUp += Click;
+            Canvas.SetLeft(ellipse, Center.X - Size / 2);
+            Canvas.SetTop(ellipse, Center.Y - Size / 2);
+            canvas.Children.Add(ellipse);
+            return ellipse;
+        }
+        
+        public override bool IsInside(Point point)
+        {
+            return (point.X - Center.X) * (point.X - Center.X) + (point.Y - Center.Y) * (point.Y - Center.Y) <= Size * Size;
+        }
+    }
+
+    class Triangle: Shape
+    {
+        public Triangle(Point center, Color color, double size)
+        {
+            Center = center;
+            Color = color;
+            Size = size;
+            Name = "Треугольник";
+        }
+
+        public Triangle(string[] vs)
+        {
+            Color = Color.FromRgb(byte.Parse(vs[1]), byte.Parse(vs[2]), byte.Parse(vs[3]));
+            Size = double.Parse(vs[4]);
+            Center = new Point(double.Parse(vs[5]), double.Parse(vs[6]));
+            Name = "Треугольник";
+        }
+
+        public override double Area()
+        {
+            return Size * Size * Math.Sqrt(3) / 4;
+        }
+
+        public override System.Windows.Shapes.Shape Draw(Canvas canvas)
+        {
+            var polygon = new System.Windows.Shapes.Polygon
+            {
+                Points = new PointCollection
+                {
+                    new Point(0, - Size / 2),
+                    new Point(Size / 2, Size / 2),
+                    new Point(- Size / 2, Size / 2)
+                },
+                Stroke = new SolidColorBrush(Color),
+                StrokeThickness = 1,
+                Fill = new SolidColorBrush(Color),
+            };
+            polygon.MouseLeftButtonUp += Click;
+            Canvas.SetLeft(polygon, Center.X - Size / 2);
+            Canvas.SetTop(polygon, Center.Y - Size / 2);            
+            canvas.Children.Add(polygon);
+            return polygon; 
+        }
+
+        public override bool IsInside(Point point)
+        {
+            return (point.X >= Center.X - Size / 2 && point.X <= Center.X + Size / 2) &&
+                   (point.Y >= Center.Y - Size / 2 && point.Y <= Center.Y + Size / 2);
+        }
+    }
+}
